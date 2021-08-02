dff8ca7f35e861882ce51b86762f6259004c3f31
André Albuquerque <andre.albuquerque@fusiondms.com.br>
2021-08-02 17:26:20 -0300

    simplificando...

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-const txt = document.querySelector('p[txt]');
-const words = ` CLI Simulator iniciado em ${new Date()}
-                \nEu sou André Albuquerque [ @andreguigo nas redes sociais ].
-                Todos os dias, um aprendizado a mais e atualmente, engenheiro de software em Recife.
-                \nCarregando experiências:
-
-                \n[ FUSION DMS ]: \n Dev pleno \n Set 2020...
-                \n[ FACULDADE NOVA ROMA ]: \n Webmaster \n Jun 2014 a Set 2020
-                \n[ INTERNE HOME CARE ]:\n Ana de Comunicação \n Out 2013 a Mai 2014
-                \n[ ... ]
-
-                \nPara mais XP consulte e siga-me no GitHub ou no LinkedIn
-                \n\npressione "r" para recarregar`;
+let words = `
+        CLI Simulator iniciado em ${new Date()}\n
+        Eu sou André Albuquerque [ @andreguigo nas redes sociais ].\n
+        Todos os dias, um aprendizado a mais e atualmente, engenheiro de software em Recife.\n\n
+        Carregando experiências:\n
+        [ FUSION DMS ]:\n Dev pleno\n Set 2020/atualmente\n
+        [ FACULDADE NOVA ROMA ]:\n Webmaster\n Jun 2014 a Set 2020\n
+        [ INTERNE HOME CARE ]:\n Comunicação\n Out 2013 a Mai 2014\n
+        [ ... ]\n\n
+        Para mais XP consulte e siga-me no GitHub ou no LinkedIn\n\n
+        pressione "r" para recarregar`;
 
 function start() {
     let i = 0;
@@ -18,17 +16,16 @@ function start() {
 
     const temp = setInterval(() => {
         loadtxt = loadtxt + words[i];
-        txt.innerText = `${loadtxt}`;
+        document.querySelector('p[txt]').innerText = `${loadtxt}`;
             
         if (i === (words.length - 1))
             clearInterval(temp);
 
         i++;        
-    }, 15);    
+    }, 25);    
 }
 
-const body = document.querySelector('body');
-body.onkeydown = function(e) {
+document.querySelector('body').onkeydown = function(e) {
     e.preventDefault();
     
     if (e.keyCode === 82)
