40767ef7d87f0e5bf218b21f0edbc3058debfc21
andreguigo <andreguigo@gmail.com>
2022-07-06 00:44:58 -0300

    Simplificando o projeto.

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # :space_invader: Simulação CLI
 
-## Animação simplificada de texto simulando uma aplicação de Interfaces de Linhas de Comando.
+## Animação simplificada de texto.
 
-O aplicativo faz parte do desenvolvimento de mini projetos pessoais.
-
-[Simulação CLI](https://andreguigo.github.io/cli-simulator_js)
+O código faz uma animação de texto simulando uma aplicação de Interfaces de Linhas de Comando.
--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-let words = `
-        CLI Simulator iniciado em ${new Date()}\n
-        Eu sou André Albuquerque [ @andreguigo nas redes sociais ].\n
-        Todos os dias, um aprendizado a mais e atualmente, engenheiro de software em Recife.\n\n
-        Carregando experiências:\n
-        [ KURIER TECNOLOGIA ]: \n Dev pleno\n Out 2021/atualmente\n
-        [ FUSION DMS ]:\n Dev pleno\n Set 2020 a Set 2021\n
-        [ FACULDADE NOVA ROMA ]:\n Webmaster\n Jun 2014 a Set 2020\n
-        [ INTERNE HOME CARE ]:\n Comunicação\n Out 2013 a Mai 2014\n
-        [ ... ]\n\n
-        Para mais XP consulte e siga-me no GitHub ou no LinkedIn\n\n
-        pressione "r" para recarregar`;
+/* **
+It is necessary to create fields for input and output of data
+Html example:
+    <p txt></p>
+    <script src="index.js"></script>
+** */
+let words = `Lorem Ipsum is simply dummy text of the printing 
+            and typesetting industry.\n\n
+            Lorem Ipsum has been the industry's standard dummy 
+            text ever since the 1500s, when an unknown printer 
+            took a galley of type and scrambled it to make a 
+            type specimen book.\n\n
+            press "r" to reload`;
 
 function start() {
     let i = 0;
@@ -18,17 +19,17 @@ function start() {
     const temp = setInterval(() => {
         loadtxt = loadtxt + words[i];
         document.querySelector('p[txt]').innerText = `${loadtxt}_`;
-            
+
         if (i === (words.length - 1))
             clearInterval(temp);
 
-        i++;        
-    }, 25);    
+        i++;
+    }, 25);
 }
 
 document.querySelector('body').onkeydown = function(e) {
     e.preventDefault();
-    
+
     if (e.keyCode === 82)
         start();
 }
